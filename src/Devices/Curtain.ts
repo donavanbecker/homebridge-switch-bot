e89ef9c8ccaffc0f0f3568af4e7367fc5491b441
Donavan Becker <donavan.becker@icloud.com>
2021-02-07 22:57:39 -0600

    Update Curtain.ts

--- src/Devices/Curtain.ts
+++ src/Devices/Curtain.ts
@@ -35,6 +35,7 @@ export class Curtain {
     // this is subject we use to track when we need to POST changes to the SwitchBot API
     this.doCurtainUpdate = new Queue(this.pushChanges.bind(this), {
       afterProcessDelay: (this.platform.config.options!.pushRate! * 1000), 
+      batchDelay: 2000,
     });
     this.curtainUpdateInProgress = false;
     this.setNewTarget = false;
